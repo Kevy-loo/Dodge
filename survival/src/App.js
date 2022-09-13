315d24f22d0a3414812234367af616c822223252
kevin-liu <liu.kevin1997@gmail.com>
2022-09-13 18:32:50 -0400

    basic setup

---
+++ survival/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import GameScreen from './components/GameScreen'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>hello</p>
+      </header>
+        <GameScreen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ survival/src/components/Character.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Character() {
+
+
+  return (
+    <div className='square' style={{
+        width: "100px",
+        height: "100px",
+        border: "1px solid black",
+        position: "absolute"
+    }}>
+    </div>
+  )
+}
+
+export default Character
