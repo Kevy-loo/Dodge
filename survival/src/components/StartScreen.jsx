658e74eafa8290b74853719410180f18baf9a841
Kevin <kevin.liu@clover.com>
2023-11-06 15:32:41 -0500

    changed title

--- survival/src/components/StartScreen.jsx
+++ survival/src/components/StartScreen.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const StartScreen = ({ onStartGame }) => {
   return (
     <div>
-      <h1>Game Title</h1>
+      <h1>Dodge the ops</h1>
       <p>Instructions: Avoid the incoming red squares to earn points. Use your arrow keys to move.</p>
       <button onClick={onStartGame}>Start</button>
     </div>
